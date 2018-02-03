1d929effc7650aff00519853749502dfdb389c79
GuiM0x <martox.lintox@gmail.com>
2018-02-03 01:02:30 +0100

    Update

--- Matrix.hpp
+++ Matrix.hpp
@@ -5,16 +5,29 @@
 #include <memory>
 #include <utility>
 
-/*template<typename> class Matrix;
+template<typename> class Matrix;
 
+template<typename T>
+std::ostream& operator<<(std::ostream&, const Matrix<T>&);
 template<typename T>
 bool operator==(const Matrix<T>&, const Matrix<T>&);
 template<typename T>
-bool operator!=(const Matrix<T>&, const Matrix<T>&);*/
+bool operator!=(const Matrix<T>&, const Matrix<T>&);
 
 template<typename T>
 class Matrix
 {
+private:
+    class Coord
+    {
+    public:
+        Coord() = delete;
+        Coord(std::size_t _row, std::size_t _col) :
+            row{_row}, col{_col} {}
+
+        std::size_t row, col;
+    };
+
 public:
     typedef typename std::vector<T>::size_type size_type;
 
@@ -22,7 +35,6 @@ public:
     Matrix();
     Matrix(size_type rows, size_type cols, const T& val = T{});
     Matrix(std::initializer_list<std::initializer_list<T>> il);
-    // TO DO ? : Matrix(const std::vector<T>&, size_type rows, size_type_cols);
     Matrix(const Matrix<T>&);
     Matrix(Matrix<T>&&) noexcept;
     Matrix<T>& operator=(const Matrix<T>&);
@@ -30,47 +42,32 @@ public:
     ~Matrix() = default;
 
     /// CAPACITY
-    size_type size() const { return m_data->size(); }
-    bool empty() const { return m_data->empty(); }
-    std::pair<size_type, size_type> dimension() const { return std::pair<size_type, size_type>(m_rows, m_cols); }
+    size_type size() const
+    { return m_data->size(); }
+
+    bool empty() const
+    { return m_data->empty(); }
+
+    Coord dimension() const
+    { return Coord(m_rows, m_cols); }
+
+    Coord getCoordByID(size_type id) const
+    { return Coord{id/m_cols, id%m_rows}; }
+
+    size_type getIDByCoord(size_type row, size_type col) const
+    { return ((row*m_cols)+col); }
 
     /// ELEMENTS ACCESS
     T& operator[](size_type id) const;
     T& operator()(size_type row, size_type col) const;
 
+    /// MODIFIERS
+    Matrix<T>& swap(Matrix<T>&);
+
     /// FRIEND OPERATORS
-    /*friend bool operator==<T>(const Matrix<T>& lhs, const Matrix<T>& rhs);
-    friend bool operator!=<T>(const Matrix<T>& lhs, const Matrix<T>& rhs);*/
-
-    friend bool operator==(const Matrix<T>& lhs, const Matrix<T> rhs)
-        { return (*(lhs.m_data) == *(rhs.m_data)); }
-
-    friend bool operator!=(const Matrix<T>& lhs, const Matrix<T>& rhs)
-        { return !(lhs == rhs); }
-
-    // (Provisoire)
-    friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix) {
-
-        if(!matrix.empty()){
-            os << "Global size : " << matrix.size() << '\n'
-               << "Total Rows  : " << matrix.m_rows << '\n'
-               << "Total Cols  : " << matrix.m_cols << '\n'
-               << '\n';
-            for(std::size_t i=0; i<matrix.m_rows; ++i){
-                for(std::size_t j=0; j<matrix.m_cols; ++j){
-                    os << const_cast<Matrix<T>&>(matrix)(i, j) << ' ';
-                }
-                os << '\n';
-            }
-            os << "--------------------------------";
-        }
-        else{
-            os << "Matrix empty.\n";
-            os << "--------------------------------";
-        }
-
-        return os;
-    }
+    friend bool operator==<T>(const Matrix<T>&, const Matrix<T>&);
+    friend bool operator!=<T>(const Matrix<T>&, const Matrix<T>&);
+    friend std::ostream& operator<<<T>(std::ostream&, const Matrix<T>&);
 
 private:
     std::shared_ptr<std::vector<T>> m_data;
@@ -89,6 +86,7 @@ Matrix<T>::Matrix() :
 {
 
 }
+
 template<typename T>
 Matrix<T>::Matrix(size_type rows, size_type cols, const T& val) :
     m_data{std::make_shared<std::vector<T>>(std::vector<T>(rows*cols, val))},
@@ -96,11 +94,12 @@ Matrix<T>::Matrix(size_type rows, size_type cols, const T& val) :
 {
 
 }
+
 template<typename T>
 Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> il) :
     Matrix(il.size(), (il.size() > 0 ? il.begin()->size() : 0))
 {
-    // TO DO : Gérer le cas où des lignes n'aurait pas le même nombre de colonnes
+    // TO DO : Gérer le cas où des lignes n'aurait pas le même nombre de colonnes (throw)
 
     std::size_t i{0};
     for(const auto& line : il){
@@ -112,6 +111,7 @@ Matrix<T>::Matrix(std::initializer_list<std::initializer_list<T>> il) :
         ++i;
     }
 }
+
 template<typename T>
 Matrix<T>::Matrix(const Matrix<T>& cpy) :
     m_data{std::make_shared<std::vector<T>>(*cpy.m_data)},
@@ -119,6 +119,7 @@ Matrix<T>::Matrix(const Matrix<T>& cpy) :
 {
 
 }
+
 template<typename T>
 Matrix<T>::Matrix(Matrix<T>&& other) noexcept :
     m_data{std::make_shared<std::vector<T>>(std::move(*other.m_data))},
@@ -129,30 +130,45 @@ Matrix<T>::Matrix(Matrix<T>&& other) noexcept :
 //////////////////////////////////////////////////////////////////
 /////// FUNCTION(S)
 template<typename T>
+Matrix<T>& Matrix<T>::swap(Matrix<T>& other)
+{
+    Matrix<T> tmp{*this};
+    *this = std::move(other);
+    other = std::move(tmp);
+
+    return *this;
+}
+
+template<typename T>
 void Matrix<T>::check(size_type i, const std::string& msg) const
 {
     if(i >= m_data->size())
         throw std::out_of_range(msg);
 }
+
 template<typename T>
 void Matrix<T>::check(size_type row, size_type col, const std::string& msg) const
 {
     if((row*m_cols)+col >= m_data->size())
-     throw std::out_of_range(msg);
+        throw std::out_of_range(msg);
 }
 //////////////////////////////////////////////////////////////////
-/////// OPERATOR(S)
+/////// ELEMENT ACCESS
 template<typename T>
-Matrix<T>& Matrix<T>::operator=(Matrix<T>&& other) noexcept
+T& Matrix<T>::operator[](size_type id) const
 {
-    if(this != &other){
-        *m_data = std::move(*other.m_data);
-        m_rows  = other.m_rows;
-        m_cols  = other.m_cols;
-    }
-
-    return *this;
+    check(id, "Access out of range");
+    return (*m_data)[id];
 }
+
+template<typename T>
+T& Matrix<T>::operator()(size_type row, size_type col) const
+{
+    check(row, col, "Access out of range");
+    return (*m_data)[(row*m_cols)+col];
+}
+//////////////////////////////////////////////////////////////////
+/////// ASSIGN OPERATORS
 template<typename T>
 Matrix<T>& Matrix<T>::operator=(const Matrix<T>& cpy)
 {
@@ -164,27 +180,55 @@ Matrix<T>& Matrix<T>::operator=(const Matrix<T>& cpy)
 
     return *this;
 }
+
 template<typename T>
-T& Matrix<T>::operator[](size_type id) const
+Matrix<T>& Matrix<T>::operator=(Matrix<T>&& other) noexcept
 {
-    check(id, "Access out of range");
-    return (*m_data)[id];
+    if(this != &other){
+        *m_data = std::move(*other.m_data);
+        m_rows  = other.m_rows;
+        m_cols  = other.m_cols;
+    }
+
+    return *this;
 }
+//////////////////////////////////////////////////////////////////
+/////// OPERATOR(S)
+//(operator<< Provisoire)
 template<typename T>
-T& Matrix<T>::operator()(size_type row, size_type col) const
+std::ostream& operator<<(std::ostream& os, const Matrix<T>& matrix)
 {
-    check(row, col, "Access out of range");
-    return (*m_data)[(row*m_cols)+col];
+    if(!matrix.empty()){
+        os << "Global size : " << matrix.size() << '\n'
+           << "Total Rows  : " << matrix.m_rows << '\n'
+           << "Total Cols  : " << matrix.m_cols << '\n'
+           << '\n';
+        for(std::size_t i=0; i<matrix.m_rows; ++i){
+            for(std::size_t j=0; j<matrix.m_cols; ++j){
+                os << const_cast<Matrix<T>&>(matrix)(i, j) << ' ';
+            }
+            os << '\n';
+        }
+        os << "--------------------------------";
+    }
+    else{
+        os << "Matrix empty.\n";
+        os << "--------------------------------";
+    }
+
+    return os;
 }
-/*template<typename T>
+
+template<typename T>
 bool operator==(const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
     return (*(lhs.m_data) == *(rhs.m_data));
 }
+
 template<typename T>
 bool operator!=(const Matrix<T>& lhs, const Matrix<T>& rhs)
 {
     return !(lhs == rhs);
-}*/
+}
 
 #endif // MATRIX_HPP_INCLUDED
