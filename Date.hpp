e3057f8bdc55a4fe55cbb5dc457e7497e50e1789
Guitox <guitox@localhost.localdomain>
2018-02-24 15:25:42 +0100

    Typo

--- Date.hpp
+++ Date.hpp
@@ -8,39 +8,39 @@
 namespace tox {
 
 enum date_format{
-	EN,
-	FR,
-	FORMAT_MAX
+    EN,
+    FR,
+    FORMAT_MAX
 };
 
 class Date
 {
 public:
-	Date() = default;
+    Date() = default;
 
-	static std::string get(int format = tox::date_format::EN);
+    static std::string get(int format = tox::date_format::EN);
 };
 
 std::string Date::get(int format)
 {
-	// Returning a time point representing the current point in time
-	auto now = std::chrono::system_clock::now();
-	// Convert this point to a timestamp
-	std::time_t now_t = std::chrono::system_clock::to_time_t(now);
-	
-	// EN Format
-	if(format == tox::date_format::EN){
-		return std::string{std::ctime(&now_t)};
-	}
-	
-	// FR Format
-	if(format == tox::date_format::FR){
-		char tmp_date[100];
-		std::strftime(tmp_date, sizeof(tmp_date), "%a %d %b %Y %T", std::localtime(&now_t));
-		return std::string{tmp_date};
-	}
-	
-	return std::string{};
+    // Returning a time point representing the current point in time
+    auto now = std::chrono::system_clock::now();
+    // Convert this point to a timestamp
+    std::time_t now_t = std::chrono::system_clock::to_time_t(now);
+
+    // EN Format
+    if(format == tox::date_format::EN){
+	    return std::string{std::ctime(&now_t)};
+    }
+
+    // FR Format
+    if(format == tox::date_format::FR){
+        char tmp_date[100];
+        std::strftime(tmp_date, sizeof(tmp_date), "%a %d %b %Y %T", std::localtime(&now_t));
+        return std::string{tmp_date};
+    }
+
+    return std::string{};
 }
 
 } // End of namespace!
